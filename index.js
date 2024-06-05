17c8714b0da3a90230efbb2224a59e91f9834d2c
PaulGrosu <162283578+PaulGrosu@users.noreply.github.com>
2024-06-06 00:35:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const options = {
+    method: 'GET',
+    headers: { accept: 'application/json' }
+};
+
+fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=f5d44386cd620242b1740754aaa259b6&language=en-US', options)
+    .then(response => response.json())
+    .then(response => {
+        const movies = response.results;
+        let carouselInner = document.querySelector('.carousel-inner');
+
+        movies.forEach((movie, index) => {
+            let activeClass = index === 0 ? 'active' : '';
+            let carouselItem = `
+                <div class="carousel-item ${activeClass}">
+                    <img class="d-block w-100" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                </div>
+            `;
+            carouselInner.innerHTML += carouselItem;
+        });
+    })
+    .catch(err => console.error(err));
